c7c4c46b5ab56c5ee4f18872c8a02e93a54034e2
Johnathan Castle <castle.johnathan@googlemail.com>
2017-11-29 10:35:38 +0000

    Updated paths

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // main app
-import App from './containers/App';
+import App from './react-files/App';
+
+//require("./containers/styles/styles.less");
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
