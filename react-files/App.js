0651d0b68a4e625d17c23490180191df17cb1997
Johnathan Castle <castle.johnathan@googlemail.com>
2017-11-29 10:38:42 +0000

    File moved

---
+++ react-files/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+import OuterController from './controllers/outer';
+
+
+export default class App extends Component {
+    render () {
+        return (<OuterController/>);
+    }
+}
