6aa0ff859bc012b6ffbe9ea2378dc3a28f6f66ee
Johnathan Castle <castle.johnathan@googlemail.com>
2017-11-29 10:37:13 +0000

    Added loaders for CSS/LESS and images

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,12 +1,27 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-var parentDir = path.join(__dirname, '../');
+const parentDir = path.join(__dirname, '../');
 
+const PATHS = {
+  root: path.join(__dirname),
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist')
+}
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+/*
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractLess = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+*/
 module.exports = {
     entry: [
-        path.join(parentDir, 'index.js'),
-        path.join(parentDir, 'containers/styles.less')
+        path.join(parentDir, 'index.js')
     ],
     module: {
         loaders: [
@@ -17,13 +32,65 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                loaders: ["style-loader", "css-loader", "less-loader"]
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                },
+                {
+                    loader: "css-loader", // translates CSS into CommonJS
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: "less-loader", // compiles Less to CSS
+                    options: {
+                        sourceMap: true
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+
+            /*
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/,
+                loader: 'url-loader?limit=10000000&name=/[path][name].[ext]',
+                include: PATHS.app + '/images'
+            },*/
+            {
+              test: /\.(gif|png|jpe?g|svg)$/i,
+              use: [
+                'file-loader',
+                {
+                  loader: 'image-webpack-loader',
+                  options: {
+                    bypassOnDebug: true,
+                  },
+                },
+              ],
             }
+            
         ]
     },
+    //plugins: [
+    //    extractLess
+    //],
+    context: path.join(__dirname, 'map-tool'),
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: '../../images',
+                to: '/images'
+            },
+        ],
+        { debug: 'info'})
+    ],
     output: {
         path: parentDir + '/dist',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
     devtool: 'source-map',
     devServer: {
