205742b6c369d6df2f04a931cc7e661754601de3
Johnathan Castle <castle.johnathan@googlemail.com>
2017-12-06 09:30:37 +0000

    Added decorater for react-dnd

--- react-files/App.js
+++ react-files/App.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 
+//import { default as TouchBackend } from 'react-dnd-touch-backend';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from 'react-dnd';
+
 import OuterController from './controllers/outer';
+import DragLayer from './controllers/drag-layer';
 
 
-export default class App extends Component {
+
+
+class App extends Component {
     render () {
         return (<OuterController/>);
     }
 }
+
+module.exports = DragDropContext(HTML5Backend)(App);
+//module.exports = DragDropContext(TouchBackend({ enableMouseEvents: true }))(App);
