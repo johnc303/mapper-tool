bfa8a658c37e75f0731f07361782a971ea35c476
Johnathan Castle <castle.johnathan@googlemail.com>
2017-12-11 12:56:45 +0000

    Added calls to the GridTileController and dynamic grid creation based on props from controller.

--- react-files/views/map.js
+++ react-files/views/map.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
 
+import GridTileController from '../controllers/grid-tile';
+
 export default class MapView extends Component
 {
-	render() {
 
-		//console.log(OuterViewStyle);
+	_renderGridTiles(){
+		let count = 0;
+		let tiles = [];
+		//console.log(this.props.tileTotals);
+		console.log("gridScale: " + this.props.gridScale);
+		const _sizes = {width: this.props.gridScale + "px", height: this.props.gridScale + "px"};
+		console.log("_sizes: ", _sizes);
 
-		var _gridStyle = {
-			backgroundSize: this.props.gridScale + "px " + this.props.gridScale + "px"
+		while (count < this.props.tileTotals.totalTiles && count < 100000)	// TODO Add better bounds checking than this!
+		{
+			tiles.push(<GridTileController sizes={_sizes} key={count} id={count}/>)
+			count++;
 		}
+		return tiles;
+	}
+
+	render() {
+		const _gridElements = this._renderGridTiles();
 
 		var _output = (
-			<div className="grid-element" style={_gridStyle}>
-				<p>Map</p>
-				<h1>{this.props.mapScale}</h1>
+			<div className="grid-element">
+				{_gridElements}
 			</div>
 		);
 		return _output;
